05ea8a7b8e29d7fc85e17927cbde28a22f13e36a
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-04-08 20:26:02 +0200

    scrolling disabled while overlay opened

--- assets/scripts/overlay-dialog.js
+++ assets/scripts/overlay-dialog.js
@@ -1,27 +1,27 @@
 // open overlay
-function openOverlay(){
-    document.getElementById('overlay').classList.remove('d-none')
-    // renderDialogCard();
+function openOverlay() {
+  document.getElementById("overlay").classList.remove("d-none");
+  renderDialogCard();
+  disableScrollingBody();
 }
 
-function closeOverlay(){
-    document.getElementById('overlay').classList.add('d-none')
+function closeOverlay() {
+  document.getElementById("overlay").classList.add("d-none");
+  enableScrollingBody();
 }
 
-async function getPokemonData(params) {
-    
-}
+async function getPokemonData(params) {}
 
 // render dialog for detail view of one pokemon
-function renderDialogCard(){
-    let currentPokemonData = 1;
-    let container = document.getElementById('dialog-container');
-    container.innerHTML = renderDialogCardTemplate();
+function renderDialogCard() {
+  let currentPokemonData = 1;
+  let container = document.getElementById("dialog-container");
+  container.innerHTML = renderDialogCardTemplate();
 }
 
-function renderDialogCardTemplate(){
-    return /*html*/`
-   <div class="card" style="background-color: ;"">
+function renderDialogCardTemplate() {
+  return /*html*/ `
+   <div class="" style="background-color: ;">
         <div class="card-header">
             <span>index</span>
             <span>name</span>
@@ -35,4 +35,12 @@ function renderDialogCardTemplate(){
         </div>
     </div>
     `;
+}
+
+function disableScrollingBody() {
+  document.body.classList.add("overflow-hidden");
+}
+
+function enableScrollingBody() {
+  document.body.classList.remove("overflow-hidden");
 }
