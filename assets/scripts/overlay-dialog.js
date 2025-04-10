2987fc151520627382ef8906dc4b89fa1377e899
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-04-10 15:03:20 +0200

    fixed abilities bug

--- assets/scripts/overlay-dialog.js
+++ assets/scripts/overlay-dialog.js
@@ -1,10 +1,10 @@
-let currentPokemon
+let currentPokemon;
 
 // open overlay
 function openOverlay(id) {
-    renderDialogCard(id);
-    document.getElementById("overlay").classList.remove("d-none");
-    disableScrollingBody();
+  renderDialogCard(id);
+  document.getElementById("overlay").classList.remove("d-none");
+  disableScrollingBody();
 }
 
 function closeOverlay() {
@@ -13,20 +13,20 @@ function closeOverlay() {
 }
 
 async function getPokemonData(id) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    // console.log(currentPokemon['sprites']['other']['official-artwork']['front_default']);
-    return currentPokemon;
+  let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  let response = await fetch(url);
+  currentPokemon = await response.json();
+  // console.log(currentPokemon['sprites']['other']['official-artwork']['front_default']);
+  return currentPokemon;
 }
 
 // render dialog for detail view of one pokemon
 async function renderDialogCard(id) {
   currentPokemon = await getPokemonData(id);
   let container = document.getElementById("dialog-container");
-//   console.log(currentPokemon['sprites']['other']['official-artwork']['front_default']);
+  //   console.log(currentPokemon['sprites']['other']['official-artwork']['front_default']);
   container.innerHTML = renderDialogCardTemplate(currentPokemon);
-  renderAbout(currentPokemon)
+  renderAbout(currentPokemon);
 }
 
 function renderDialogCardTemplate(currentPokemon) {
@@ -43,12 +43,14 @@ function renderDialogCardTemplate(currentPokemon) {
                   <span>${currentPokemon.id}</span>
                 </div>
                 <div id="dialog-types-container" class="types"></div>
-                <img class="dialog-pic" src="${currentPokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']}" alt="picture of name">
+                <img class="dialog-pic" src="${
+                  currentPokemon["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
+                }" alt="picture of name">
             </div>
             <div class="detail-info">
                 <nav class="nav-details" aria-label="Navigation through detail info">
                     <button onclick="renderAbout()">about</button>
-                    <button onclick="renderAboutStats()">stats</button>
+                    <button onclick="renderStats()">stats</button>
                     <button onclick="renderAbout(${currentPokemon.id})">evolution</button>
                     <button onclick="renderAbout(${currentPokemon.id})">moves</button>
                 </nav>
@@ -59,46 +61,57 @@ function renderDialogCardTemplate(currentPokemon) {
     `;
 }
 
-function renderAbout(){
-  let container = getElementHelper('detail-info-container');
+function renderAbout() {
+  let container = getElementHelper("detail-info-container");
   container.innerHTML = renderAboutTemplate(currentPokemon);
-  renderAboutAbilities(currentPokemon['abilities'])
-
+  renderAboutAbilities(currentPokemon["abilities"]);
 }
 
-function renderAboutTemplate(currentPokemon){
-  return /*html*/`
-    <p>Species ${capitalizeFirstLetter(currentPokemon['species']['name'])}</p>
-    <p>Height ${currentPokemon['height'] * 10}cm</p>
-    <p>Weight ${currentPokemon['height']}kg</p>
-    <p id="abilities">Abilities </p>
-  `
+function renderAboutTemplate(currentPokemon) {
+  return /*html*/ `
+    <p>Species ${capitalizeFirstLetter(currentPokemon["species"]["name"])}</p>
+    <p>Height ${currentPokemon["height"] * 10}cm</p>
+    <p>Weight ${currentPokemon["height"]}kg</p>
+    <div class="abilities">
+      <span>Abilities</span>
+      <div id="abilities-container"></div>
+    </div>
+  `;
 }
 
-function renderAboutAbilities(abilities){
-  let container = getElementHelper('detail-info-container');
-  
+function renderAboutAbilities(abilities) {
+  let container = getElementHelper("abilities-container");
+
   for (let i = 0; i < abilities.length; i++) {
     const abilityName = abilities[i].ability.name;
-    container.innerHTML += /*html*/`
-      <span>${capitalizeFirstLetter(abilityName)}</span>
+    container.innerHTML += /*html*/ `
+      <p>${capitalizeFirstLetter(abilityName)}</p>
     `;
   }
 }
 
-function renderAboutStats(){
-  let container = getElementHelper('detail-info-container');
-  let stats = currentPokemon.stats;
-  container.innerHTML = "";  
- 
-  for (let i = 0; i < stats.length; i++) {
-    container.innerHTML += /*html*/`
+function renderStats() {
+  let container = getElementHelper("detail-info-container");
+  container.innerHTML = "";
+
+  for (let i = 0; i < currentPokemon.stats.length; i++) {
+    container.innerHTML += /*html*/ `
       <p>
-        <span>${currentPokemon['stats'][i]['stat']['name']}</span>
-        <span>${currentPokemon['stats'][i]['base_stat']}</span>
+        <span>${currentPokemon["stats"][i]["stat"]["name"]}</span>
+        <span>${currentPokemon["stats"][i]["base_stat"]}</span>
       </p>
-    `    
+    `;
   }
+}
+
+function renderAboutMoves() {
+  let container = getElementHelper("detail-info-container");
+  container.innerHTML = "";
+
+  for (let i = 0; i < currentPokemon.moves.length; i++) {
+    
+  }
+
 }
 
 function disableScrollingBody() {
